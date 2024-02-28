a95969cfe534b149f8928da2885f888e166eeedc
Bart Massey <bart.massey@gmail.com>
2024-02-28 13:39:22 -0800

    finished first prototype; still not reliable

--- README.md
+++ README.md
@@ -10,13 +10,14 @@ MB2. Enabling that interrupt allows the IMU to notify the
 MB2 when it should pay attention.
 
 However, this interrupt pin is shared by the Interface MCU (IMCU)
-on the MB2. At program startup
-
-## Build and Run
+on the MB2. At program startup the IMCU needs to release the
+pin so that the IMU can interrupt with it. This turns out to
+be a bit buggy.
 
 ## Acknowledgements
 
-
+Thanks to the students of PSU CS 410P/510 Rust Embedded
+Winter 2024 that did most of the work of figuring stuff out.
 
 Thanks to the `microbit-v2` and `lsm303agr` crate authors
 for fantastic work.
--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,14 @@ use cortex_m::asm;
 
 use lsm303agr::*;
 
+use critical_section_lock_mut::LockMut;
+
+static P_GPIOTE: LockMut<Gpiote> = LockMut::new();
+
 #[interrupt]
 fn GPIOTE() {
     rprintln!("gpiote interrupt");
+    P_GPIOTE.with_lock(|gpiote| gpiote.channel0().reset_events());
 }
 
 #[entry]
@@ -42,31 +47,39 @@ fn main() -> ! {
     // code.
     rprintln!("clearing i2c_int_int");
     // Endpoint 0x70 is the IMCU. The User Event message is
-    // 5 bytes.
-    let mut buf = [0u8; 4];
-    i2c.read(0x70, &mut buf).unwrap();
-    match &buf {
-        &[0x20, 0x39, 0x0, 0x0] => {
-            rprintln!("got 'busy' message");
-        }
-        &[0x11, 0x09, 0x1, cause] => {
-            if !(1..=3).contains(&cause) {
-                panic!("unexpected 'user event' cause {:x}", cause);
-            }
-            rprintln!("got 'user event' message {:x}", cause);
-        }
-        _ => panic!("unexpected message {:x?}", buf),
-    }
+    // 4 bytes. The Busy message is 2 bytes.
     let mut delay = Delay::new(board.SYST);
-    let mut msecs = 1000;
+    delay.delay_ms(1000_u16);
+    loop {
+        if interrupt_pin.is_high().unwrap() {
+            break;
+        }
+        let mut buf = [0u8; 255];
+        i2c.read(0x70, &mut buf).unwrap();
+        match &buf {
+            &[0x20, 0x39, 0x0, ..] => {
+                rprintln!("got 'busy' message");
+                delay.delay_ms(1000_u16);
+            }
+            &[0x11, 0x09, 0x1, cause, ..] => {
+                if !(1..=3).contains(&cause) {
+                    panic!("unexpected 'user event' cause {}", cause);
+                }
+                rprintln!("got 'user event' message {}", cause);
+                break;
+            }
+            _ => panic!("unexpected message {:x?}", buf),
+        }
+    }
+    let mut msecs = 0;
     while interrupt_pin.is_low().unwrap() {
-        if msecs == 0 {
-            panic!("interrupt pin stuck low for 1000 ms");
+        if msecs >= 5000 {
+            panic!("interrupt pin stuck low for {} ms", msecs);
         }
         delay.delay_ms(1u16);
-        msecs -= 1;
+        msecs += 1;
     }
-    rprintln!("interrupt went high in {}ms", 1000 - msecs);
+    rprintln!("interrupt went high in {}ms", msecs);
 
     rprintln!("continuing setup");
 
@@ -87,10 +100,14 @@ fn main() -> ! {
         .enable_interrupt();
     channel0.reset_events();
 
+    P_GPIOTE.init(gpiote);
+
     unsafe { pac::NVIC::unmask(pac::Interrupt::GPIOTE) };
     pac::NVIC::unpend(pac::Interrupt::GPIOTE);
 
     rprintln!("setup complete");
+
+    lsm303.acc_enable_interrupt(Interrupt::DataReady1).unwrap();
 
     loop {
         asm::wfe();
